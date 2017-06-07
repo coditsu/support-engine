e5bd27ec9a838db44e860ab2a52bb3719f2ae5f9
Tomasz Pajor <tomek@polishgeeks.com>
2017-06-07 11:49:06 +0200

    add more specs

---
+++ spec/lib/support_engine/git/repo_builder/no_master_mirror_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe SupportEngine::Git::RepoBuilder::NoMasterMirror do
+  describe 'BOOTSTRAP_CMD' do
+    subject(:bootstrap) { described_class.bootstrap }
+
+    before do
+      expect(described_class).to receive(:destroy)
+      expect(
+        SupportEngine::Shell
+      ).to receive(:call).with(described_class::BOOTSTRAP_CMD) { 'test' }
+    end
+
+    it { expect(bootstrap).to eq('test') }
+  end
+end
