4b2e7fb9c436149af5945d3ebbd8b14feeb3a4ce
Tomasz Pajor <tomek@polishgeeks.com>
2017-06-10 12:53:32 +0200

    allow to pass options to Shell.call

--- lib/support_engine/shell/git.rb
+++ lib/support_engine/shell/git.rb
@@ -8,13 +8,17 @@ module SupportEngine
         # Executes a given git command in path location
         # @param path [String] path of a current repository build
         # @param command [Symbol] name of a git command that we want to execute
-        # @param arguments [String] all arguments accepted by this git command
+        # @param command_options [String] options that we want to pass to command
+        # @param options [String] options that we want to pass to Shell.call
         # @raise [SupportEngine::Errors::FailedShellCommand] raised when git command didn't
         #   run with success
         # @example
-        #   shell('/home/builds/91', :log, "-n 1 --word-diff=porcelain --date=raw #{location}")
-        def call_in_path(path, command, options)
-          result = SupportEngine::Shell::Utf8.call("git -C #{path} #{command} #{options}")
+        #   call_in_path('/home/91', :log, "-n 1 --word-diff=porcelain --date=raw #{location}")
+        def call_in_path(path, command, command_options, options = { raise_on_invalid_exit: true })
+          result = SupportEngine::Shell::Utf8.call(
+            "git -C #{path} #{command} #{command_options}",
+            options
+          )
 
           result[:stdout].split("\n")
         end
--- lib/support_engine/shell/yarn.rb
+++ lib/support_engine/shell/yarn.rb
@@ -7,17 +7,23 @@ module SupportEngine
       class << self
         # Executes a given yarn command
         # @param command [String] shell command that we want to execute
-        # @param options [String] options for command
+        # @param command_options [String] options that we want to pass to command
+        # @param options [String] options that we want to pass to Shell.call
         # @return [Hash] Shell.call execution hash
-        def call(command, options)
-          Shell.call("yarn run --silent #{command} -- #{options}")
+        def call(command, command_options, options = { raise_on_invalid_exit: true })
+          Shell.call("yarn run --silent #{command} -- #{command_options}", options)
         end
 
         # @param path [String, Pathname] path to a place where git repo is
         # @param command [String] command that we want to execute in path context
+        # @param command_options [String] options that we want to pass to command
+        # @param options [String] options that we want to pass to Shell.call
         # @return [Hash] Shell.call execution hash
-        def call_in_path(path, command, options)
-          Shell.call("cd #{path.to_s.shellescape} && yarn run --silent #{command} -- #{options}")
+        def call_in_path(path, command, command_options, options = { raise_on_invalid_exit: true })
+          Shell.call(
+            "cd #{path.to_s.shellescape} && yarn run --silent #{command} -- #{command_options}",
+            options
+          )
         end
       end
     end
