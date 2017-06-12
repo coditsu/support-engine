57624cb2cb5f70b96148ec8e7d2b30ec2876f684
Tomasz Pajor <tomek@polishgeeks.com>
2017-06-12 11:44:45 +0200

    run with sudo

--- bin/install/yarn.sh
+++ bin/install/yarn.sh
@@ -12,9 +12,9 @@ install ()
 	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
 	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
         if [ -z "$(dpkg -l|grep yarn)" ]; then
-            apt-get update && apt-get install yarn;
+            sudo apt-get update && sudo apt-get install yarn;
         else
-            apt-get upgrade yarn;
+            sudo apt-get upgrade yarn;
         fi
     elif [ $PLATFORM == "Darwin" ]; then
         if [ -z "$(brew list|grep yarn)" ]; then
