00833d0fbcddc7f270ee82cc11a850fb974ce83f
Maciej Mensfeld <maciej@mensfeld.pl>
2017-07-15 13:46:06 +0200

    better branch picking

--- lib/support_engine/git/commits.rb
+++ lib/support_engine/git/commits.rb
@@ -28,10 +28,15 @@ module SupportEngine
           result = SupportEngine::Shell.call_in_path(path, cmd.join(' '))
           fail_if_invalid(result)
 
+          # We need to know the main head of the repo, for branch picking
+          # In case there are multiple branches containing same commit, we prioritize
+          # head as the owner of a commit
+          head = Ref.head(path)[:stdout].gsub("\n", '')
+
           result[:stdout]
             .split('~')
             .delete_if(&:empty?)
-            .map(&method(:build_commit))
+            .map { |commit| build_commit(commit, head) }
         end
 
         # Fetches newest commit for each day with day details
@@ -57,7 +62,7 @@ module SupportEngine
           result[:stdout]
             .split("\n")
             .delete_if(&:empty?)
-            .map(&method(:build_commit))
+            .map { |commit| build_commit(commit, nil) }
         end
 
         private
@@ -74,14 +79,14 @@ module SupportEngine
         # Builds a commit hash with basic details about single commit
         # @param raw_commit_data [String] raw string that describes a single commit
         # @return [Hash] hash with commit details (commit hash, date and branch)
-        def build_commit(raw_commit_data)
+        def build_commit(raw_commit_data, head)
           data = raw_commit_data.split("\n")
           base = data.shift.split('|')
 
           {
             commit_hash: base[1],
             committed_at: Time.zone.parse(base[0]),
-            branch: resolve_branch(data, base[1])
+            branch: resolve_branch(data, base[1], head)
           }
         end
 
@@ -92,14 +97,24 @@ module SupportEngine
         # @param each_ref [Array<String>] array with for-each-ref results
         # @return [String, nil] nil if no branch detected for a given commit or a branch name
         #   if found
-        def resolve_branch(each_ref, commit_hash)
+        def resolve_branch(each_ref, commit_hash, head)
+          candidates = each_ref
+
+          # We prioritize head branches as main branches of a commit if they are in the head
+          return head if candidates.any? do |candidate|
+            candidate.include?('origin/HEAD')
+            candidate.include?("refs/heads/#{head}")
+          end
+
           # First we select only those branches that not only contain but have our commit as
-          # the last one (when we are checkout it will work like that as well)
+          # the last one (when we are checkout it will work like that as well). For the head
+          # commits this is the most accurate
           candidates = each_ref.select { |ref| ref.start_with?(commit_hash) }
+          # And if non like that, we just fallback to all branches and pick the first one
+          candidates = each_ref if candidates.empty?
+
           # We pick only the branch name part as the bash command result contains some noise
           candidates.map! { |candidate| candidate.split("\t").last }
-          # We remove head reference as it does not tell us anything
-          candidates.delete_if { |candidate| candidate.start_with?('refs/remotes/origin/HEAD') }
 
           # And we pick the first one with and sanitize it to get only the branch name
           candidates
