593ef7cfbba915573fa768e905e19d3fed582976
Maciej Mensfeld <maciej@mensfeld.pl>
2017-09-08 18:48:02 +0200

    spec remarks

--- spec/lib/support_engine/git/repo_builder/yarn_spec.rb
+++ spec/lib/support_engine/git/repo_builder/yarn_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe SupportEngine::Git::RepoBuilder::Yarn do
   describe 'BOOTSTRAP_CMD' do
     subject(:bootstrap) { described_class.bootstrap }
 
-    before do
-      expect(described_class).to receive(:destroy)
-      expect(
-        SupportEngine::Shell
-      ).to receive(:call).with(described_class::BOOTSTRAP_CMD) { 'test' }
-    end
+    let(:shell_args) { described_class::BOOTSTRAP_CMD }
 
-    it { expect(bootstrap).to eq('test') }
+    it 'expect to remove previous repo, build new and return path to it' do
+      expect(described_class).to receive(:destroy)
+      expect(SupportEngine::Shell).to receive(:call).with(shell_args).and_return('test')
+      expect(bootstrap).to eq('test')
+    end
   end
 end
