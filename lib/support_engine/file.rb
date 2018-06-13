ad4cfb8694e9e498e541c261e5d9a784bf0c0557
Maciej Mensfeld <maciej@mensfeld.pl>
2018-06-13 10:00:24 +0200

    weird path encoding fix

--- lib/support_engine/file.rb
+++ lib/support_engine/file.rb
@@ -6,14 +6,16 @@ module SupportEngine
     class << self
       # Regexp to select encoding details from file -bi command resulsts
       CHARSET_DETECT_REGEXP = /charset=(.*)\n/
+      # Default fallback encoding
+      DEFAULT_ENCODING = 'utf-8'
 
       # We need to detect encoding because not all files are in utf8
       # @param path [String] path to a file for which we want to check encoding
       # return [String] encoding of a given file
       def encoding(path)
         command_options = Gem::Platform.local.os == 'darwin' ? '-bI' : '-bi'
-        result = SupportEngine::Shell.call("file #{command_options} #{path}")
-        result[:stdout].match(CHARSET_DETECT_REGEXP)&.captures&.first || 'utf-8'
+        result = SupportEngine::Shell.call("file #{command_options} #{Shellwords.escape(path)}")
+        result[:stdout].match(CHARSET_DETECT_REGEXP)&.captures&.first || DEFAULT_ENCODING
       end
     end
   end
--- spec/lib/support_engine/file_spec.rb
+++ spec/lib/support_engine/file_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe SupportEngine::File do
     let(:path) { File.join(SupportEngine::Git::RepoBuilder::Master.location, 'master.rb') }
 
     it { expect(file_encoding).to eq('us-ascii') }
+
+    context 'when file name includes weird characters' do
+      let(:path) { 'weird () - $%^&*file.rb' }
+
+      it 'expect to work without problems' do
+        expect(file_encoding).to eq('utf-8')
+      end
+    end
   end
 end
