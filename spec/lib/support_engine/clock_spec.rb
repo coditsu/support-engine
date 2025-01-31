cf14fb6a4a8c788f25acbf5860fec59b0420a73f
Tomasz Pajor <tomek@polishgeeks.com>
2025-01-31 09:34:15 +0100

    upgrade to rails 7 (#162)

--- spec/lib/support_engine/clock_spec.rb
+++ spec/lib/support_engine/clock_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe_current do
   let(:instance) { described_class.new }
 
   describe '#measure' do
-    subject(:measure) { instance.measure { sleep(0.01) } }
+    subject(:measure) { instance.measure { sleep(0.5) } }
 
     it { is_expected.to be_a(Array) }
-    it { expect(measure).to eq([0, 10]) }
+    it { expect(measure[0]).to be_a(Numeric) }
+    it { expect(measure[1]).to be_within(500).of(500) }
   end
 end
